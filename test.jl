26ec8deb52777ab7c65c2fdf2272acb46dcabc16
Isabella Scalzi <isabellascalzi@gmail.com>
2018-12-06 14:08:58 +1030

    Created test.jl
    and played around with some shit

---
+++ test.jl
@@ -0,0 +1,11 @@
+using Example
+
+function sayhello(name)
+    println("Hello $name, have you ever questioned the nature of your reality?")
+    #(name == "Isabella") ? println("these violent delights have violent ends") : println("Ur name is shit")
+end
+
+
+
+
+sayhello("Isabdfhsdella")
